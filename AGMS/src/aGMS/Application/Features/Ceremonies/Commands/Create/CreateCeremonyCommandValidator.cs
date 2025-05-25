3be2bee341148255eb2efbb2dbfafd013f04d556
Nadir Can <nadircan2003@gmail.com>
2025-05-25 16:40:33 +0300

    crud fixed

--- AGMS/src/aGMS/Application/Features/Ceremonies/Commands/Create/CreateCeremonyCommandValidator.cs
+++ AGMS/src/aGMS/Application/Features/Ceremonies/Commands/Create/CreateCeremonyCommandValidator.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using Domain.Enums;
 
 namespace Application.Features.Ceremonies.Commands.Create;
 
@@ -9,7 +10,7 @@ public class CreateCeremonyCommandValidator : AbstractValidator<CreateCeremonyCo
         RuleFor(c => c.CeremonyDate).NotEmpty();
         RuleFor(c => c.CeremonyLocation).NotEmpty();
         RuleFor(c => c.CeremonyDescription).NotEmpty();
-        RuleFor(c => c.CeremonyStatus).NotEmpty();
+        RuleFor(c => c.CeremonyStatus).IsInEnum().WithMessage("Geçerli bir tören durumu seçiniz (0: Beklemede, 1: Onaylandı, 2: Reddedildi)");
         RuleFor(c => c.AcademicYear).NotEmpty();
         RuleFor(c => c.StudentAffairsId).NotEmpty();
     }
--- AGMS/src/aGMS/Application/Features/Ceremonies/Commands/Update/UpdateCeremonyCommandValidator.cs
+++ AGMS/src/aGMS/Application/Features/Ceremonies/Commands/Update/UpdateCeremonyCommandValidator.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using Domain.Enums;
 
 namespace Application.Features.Ceremonies.Commands.Update;
 
@@ -10,7 +11,7 @@ public class UpdateCeremonyCommandValidator : AbstractValidator<UpdateCeremonyCo
         RuleFor(c => c.CeremonyDate).NotEmpty();
         RuleFor(c => c.CeremonyLocation).NotEmpty();
         RuleFor(c => c.CeremonyDescription).NotEmpty();
-        RuleFor(c => c.CeremonyStatus).NotEmpty();
+        RuleFor(c => c.CeremonyStatus).IsInEnum().WithMessage("Geçerli bir tören durumu seçiniz (0: Beklemede, 1: Onaylandı, 2: Reddedildi)");
         RuleFor(c => c.AcademicYear).NotEmpty();
         RuleFor(c => c.StudentAffairsId).NotEmpty();
     }
